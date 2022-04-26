65f593a0e2859dd25f377b28286e6eb0da1ae425
AndrewChumakov <andrew.chumakov12@gmail.com>
2022-04-27 01:42:00 +0300

    сверстал все компоненты

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h2>Учет сотрудников в компании N</h2>
+            <h3>Общее число сотрудников:</h3>
+            <h3>Премию получат:</h3>
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/components/app/app.js
@@ -0,0 +1,36 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+
+
+
+function App() {
+  const data = [
+    {name: "John", salary: 800, increase: true},
+    {name: "Alex", salary: 900, increase: false},
+    {name: "John", salary: 1000, increase: true},
+    {name: "John", salary: 1000, increase: true},
+    {name: "John", salary: 1000, increase: true},
+  ];
+
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
